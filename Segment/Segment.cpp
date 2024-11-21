29b08e3d8b69ea54f3a0668e1b75f15b389c0d59
Gunnar Knutson <gunnar.knutson@proton.me>
2024-11-21 11:18:58 -0800

    Minor reformat, add constructor for adding char

--- Segment/Segment.cpp
+++ Segment/Segment.cpp
@@ -4,16 +4,18 @@
 
 using std::string;
 
-Segment * Segment::add(const string &str, const size_t size) {
+Segment *Segment::add(const string &str, const size_t size) {
     content.append(str);
     len += size;
 
     return this;
 }
 
-Segment * Segment::add(const string &str) { return add(str, str.size()); }
+Segment *Segment::add(const string &str) { return add(str, str.size()); }
 
-Segment * Segment::addForm(const std::string &str) { return add(str, 0); }
+Segment *Segment::add(const char chr) { return add({chr}, 1); }
+
+Segment *Segment::addForm(const std::string &str) { return add(str, 0); }
 
 std::string Segment::getContent() const { return content; }
 
--- Segment/Segment.h
+++ Segment/Segment.h
@@ -9,8 +9,12 @@ class Segment {
     size_t len = 0;
 
 public:
-    Segment *add(const string& str, const size_t size);
-    Segment *add(const string& str);
+    Segment *add(const string &str, const size_t size);
+
+    Segment *add(const string &str);
+
+    Segment *add(const char chr);
+
     Segment *addForm(const std::string &str);
 
     [[nodiscard]] std::string getContent() const;
