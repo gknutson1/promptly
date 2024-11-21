9d005f11200f38b40f89f7de98a6da1e3affdcfe
Gunnar Knutson <gunnar.knutson@proton.me>
2024-11-21 11:40:48 -0800

    Make username red if root, blue otherwise

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 #include <unistd.h>
 
 #include "Segment/Segment.h"
+#include "term.h"
 
 void addUserHost(Segment &seg) {
+
+    if (getuid() == 0) { seg.addForm(term::RED); }
+    else { seg.addForm(term::LIGHT_BLUE); }
+
+    seg.add(getlogin())->addForm(term::RESET)->add('@');
+
     char hostname[_SC_HOST_NAME_MAX];
     gethostname(hostname, _SC_HOST_NAME_MAX);
 
-    seg.add(getlogin())
-        ->add('@')
-        ->add(hostname);
+    seg.add(hostname);
 }
 
 int main() {
