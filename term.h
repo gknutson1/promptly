44591c9cf619d854ea123aec3b9ce1cf04d745f6
Gunnar Knutson <gunnar.knutson@proton.me>
2024-11-23 20:38:32 -0800

    Use macro to generate terminal codes

--- term.h
+++ term.h
@@ -2,14 +2,18 @@
 
 #include <string>
 
+#define genSEQ(str) (std::string("\033[") + str + "m")
+#define genFG(str) (genSEQ("38;5;" + str))
+#define genBG(str) (genSEQ("48;5;" + str))
+
 class fore {
 public:
-    static constexpr std::string WHITE      = "\033[38;5;7m";
-    static constexpr std::string LIGHT_BLUE = "\033[38;5;6m";
-    static constexpr std::string DARK_BLUE  = "\033[38;5;4m";
-    static constexpr std::string RED        = "\033[38;5;1m";
-    static constexpr std::string YELLOW     = "\033[38;5;3m";
     static constexpr std::string RESET      = "\033[0m";
+    static constexpr std::string WHITE      = genFG("7");
+    static constexpr std::string LIGHT_BLUE = genFG("6");
+    static constexpr std::string DARK_BLUE  = genFG("4");
+    static constexpr std::string RED        = genFG("1");
+    static constexpr std::string YELLOW     = genFG(DEFAULT_FG);
 };
 
 class back {
