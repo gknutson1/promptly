09047f3836d157d9dcd53ffe2033e9f8b966eda6
Gunnar Knutson <gunnar.knutson@proton.me>
2024-11-27 15:46:33 -0800

    reformat term - use struct, `string` instead of `std::string`

--- term.h
+++ term.h
@@ -37,40 +37,35 @@ static constexpr size_t strlen(string str) {
     return result;
 }
 
-class fore {
-public:
-    static constexpr std::string WHITE      = genFG("7");
-    static constexpr std::string LIGHT_BLUE = genFG("6");
-    static constexpr std::string DARK_BLUE  = genFG("4");
-    static constexpr std::string RED        = genFG("1");
-    static constexpr std::string YELLOW     = genFG("3");
-    static constexpr std::string DEFAULT    = genFG(DEFAULT_FG);
-    static constexpr std::string BG_DEFAULT = genFG(DEFAULT_BG);
+struct fore {
+    static constexpr string WHITE      = genFG("7");
+    static constexpr string LIGHT_BLUE = genFG("6");
+    static constexpr string DARK_BLUE  = genFG("4");
+    static constexpr string RED        = genFG("1");
+    static constexpr string YELLOW     = genFG("3");
+    static constexpr string DEFAULT    = genFG(DEFAULT_FG);
+    static constexpr string BG_DEFAULT = genFG(DEFAULT_BG);
 };
 
-class back {
-public:
-    static constexpr std::string WHITE      = "\033[48;5;7m";
-    static constexpr std::string DEFAULT    = genBG(DEFAULT_BG);
+struct back {
+    static constexpr string WHITE      = "\033[48;5;7m";
+    static constexpr string DEFAULT    = genBG(DEFAULT_BG);
 };
 
-class chars {
-public:
+struct chars {
     // These are strings, not chars, because they are mostly two-byte Unicode sequences,
     // And it is (probably) easier/more preformant to store them as a string,
     // instead of a char16_t that will need to be converted back into a 2-byte u8 char
     // every time it needs to be added to a segment.
-    static constexpr std::string L_SEP = "\ue0b3";
-    static constexpr std::string M_SEP = "\u00b7";
-    static constexpr std::string R_SEP = "\ue0b1";
+    static constexpr string R_SEP = "\ue0b3";
+    static constexpr size_t R_SEP_LEN = strlen(R_SEP);
+    static constexpr string M_SEP = "\u00b7";
+    static constexpr string L_SEP = "\ue0b1";
+    static constexpr size_t L_SEP_LEN = strlen(L_SEP);
 };
 
-class ctrl {
-public:
-    static constexpr std::string RESET = genSEQ("0");
-    static constexpr std::string RESET_FG = genSEQ("39");
-    static constexpr std::string RESET_BG = genSEQ("49");
+struct ctrl {
+    static constexpr string RESET = genSEQ("0");
+    static constexpr string RESET_FG = genSEQ("39");
+    static constexpr string RESET_BG = genSEQ("49");
 };
-
-static std::string R_DIV = back::DEFAULT + " " + fore::BG_DEFAULT + ctrl::RESET_BG + "\ue0b0" + fore::DEFAULT;
-static std::string L_DIV = ctrl::RESET_BG + fore::BG_DEFAULT + "\ue0b2" + fore::DEFAULT + back::DEFAULT + " ";
