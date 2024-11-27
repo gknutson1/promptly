8c3db70e425ba3a9555999a88a75ae5aab7108f1
Gunnar Knutson <gunnar.knutson@proton.me>
2024-11-27 15:44:00 -0800

    Add strlen function

--- term.h
+++ term.h
@@ -2,11 +2,40 @@
 
 #include <string>
 
+using std::string;
+
 #define genSEQ(str) ("\033[" str "m")
 #define genFG(str) (genSEQ("38;5;" str))
 #define genBG(str) (genSEQ("48;5;" str))
 #define DEFAULT_FG "246"
 #define DEFAULT_BG "236"
+
+/**
+ * Calculate the length of a string as number of characters displayed.
+ * Handles Unicode multibyte chars as well as terminal escape sequences
+ * @param str The string to count
+ * @return The number of characters that would be displayed if the string were printed
+ */
+static constexpr size_t strlen(string str) {
+    size_t result = 0;
+
+    bool term_seq = false;
+
+    for (const char chr : str) {
+        // terminal sequences are exited when we reach an 'm' character
+        if (term_seq) { if (chr == 'm') term_seq = false; }
+        // terminal sequences start with the '\033' char
+        else if (chr == '\033') {term_seq = true; }
+
+        // Any byte that starts with the bits "10" is an extension
+        // of a utf-s character (the first bit in a multibyte utf-8
+        // char starts w/ "11", and a non multibyte char starts w/ "0")
+        // so we skip everything that starts w/ "10"
+        else if ( (chr & 0xc0) != 0x80) result++;
+    }
+
+    return result;
+}
 
 class fore {
 public:
