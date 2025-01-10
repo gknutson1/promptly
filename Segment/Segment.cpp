084a3702b99452394a9cf7e7b1e7b3e11db59f3d
Gunnar Knutson <gunnar.knutson@proton.me>
2025-01-10 12:21:56 -0800

    Add comments

--- Segment/Segment.cpp
+++ Segment/Segment.cpp
@@ -6,15 +6,20 @@ size_t Segment::getLen() const {
     size_t size = base_len;
 
     for (const auto &element : elements) { size += sep_len + element.getLen(); }
+
+    // We hae added the length of sep for every element, but there are one fewer
+    // separators that there are elements, so we need to subtract sep_len once
     return size - sep_len;
 }
 
 string Segment::getContent() const {
     string s = L_DIV;
+    // Insert the first item without a seperator
     auto iter = elements.begin();
     s += iter->getContent();
     ++iter;
 
+    // Insert all other items with a seperator
     for (; iter != elements.end(); ++iter) {
         s += sep + iter->getContent();
     }
