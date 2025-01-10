7e224186365f3adc064e68a6f9b232505b0b6af9
Gunnar Knutson <gunnar.knutson@proton.me>
2025-01-10 10:39:50 -0800

    Update Segment to use range-for loop

--- Segment/Segment.cpp
+++ Segment/Segment.cpp
@@ -4,30 +4,12 @@ using std::string;
 
 size_t Segment::getLen() const {
     size_t size = base_len;
-
-    auto iter = elements.begin();
-    size += iter->getLen();
-    ++iter;
-
-    while (iter != elements.end()) {
-        size += sep_len + iter->getLen();
-        ++iter;
-    }
-
+    for (const auto& item : elements) { size += item.getLen();; }
     return size;
 }
 
 string Segment::getContent() const {
-    auto iter = elements.begin();
-    string s = L_DIV + iter->getContent();
-    ++iter;
-
-    while (iter != elements.end()) {
-        s += sep + iter->getContent();
-        ++iter;
-    }
-
-    s += R_DIV;
-
-    return s;
+    string s = L_DIV;
+    for (const auto& item : elements) { s += item.getContent(); }
+    return s + R_DIV;
 }
